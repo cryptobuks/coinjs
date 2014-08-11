a54eaaae88057d1d51e1f1488bb88498e332436b
Nathan Marley <nathan.marley@gmail.com>
2014-08-11 13:24:11 -0500

    enable one-line switch of projects in current.js

--- current.js
+++ current.js
@@ -1,11 +1,27 @@
 (function () {
   var coin = require('./coin')
 
-  var conn_opts = {
+  var ltc_testnet_opts = {
     port: coin.Projects.litecoin.testnet.port,
     username: process.env.LITECOINRPC_USER,
     password: process.env.LITECOINRPC_PASS,
   }
+
+  var btc_testnet_opts = {
+    port: coin.Projects.bitcoin.testnet.port,
+    username: process.env.BITCOINRPC_USER,
+    password: process.env.BITCOINRPC_PASS,
+  }
+
+  var drk_testnet_opts = {
+    port: coin.Projects.darkcoin.testnet.port,
+    username: process.env.DARKCOINRPC_USER,
+    password: process.env.DARKCOINRPC_PASS,
+  }
+
+  // var conn_opts = drk_testnet_opts
+  var conn_opts = btc_testnet_opts
+
   module.exports = new coin.JSONRPC(conn_opts)
 
 })()
