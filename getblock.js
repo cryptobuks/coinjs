df08db1415a1e092c4809c292adef2b3c7d506d2
Nathan Marley <nathan.marley@gmail.com>
2014-08-13 14:17:17 -0500

    convert getblock to use current.js

--- getblock.js
+++ getblock.js
@@ -1,23 +1,19 @@
-var coin = require('./coin')
+var jsonrpc = require('./current')
 var util = require('util')
 
-var btc_testnet_opts = {
-  port: coin.Projects.bitcoin.testnet.port,
-  username: process.env.BITCOINRPC_USER,
-  password: process.env.BITCOINRPC_PASS,
-}
-var btc_test_rpc = new coin.JSONRPC(btc_testnet_opts)
-
-var blockno = +process.argv[2] || 0
-
-btc_test_rpc.getblockhash(blockno, function (err, hash) {
+var handleError = function (err) {
   if (err) {
     console.error(err)
     throw(err)
   }
-  else {
-    btc_test_rpc.getblock(hash, function (err, block) {
-      console.log(util.inspect(block))
-    })
-  }
+}
+
+var blockno = +process.argv[2] || 0
+
+jsonrpc.getblockhash(blockno, function (err, hash) {
+  handleError(err)
+  jsonrpc.getblock(hash, function (err, block) {
+    handleError(err)
+    console.log(util.inspect(block))
+  })
 })
