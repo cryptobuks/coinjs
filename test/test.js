f60aae29bba20c18ea90acdc4221739ac4d19c6d
Nathan Marley <nathan.marley@gmail.com>
2014-08-14 21:49:12 -0500

    assert that methods are implemented

--- test/test.js
+++ test/test.js
@@ -37,5 +37,81 @@ describe('Coin', function () {
     it('should have a JSONRPC() function', function () {
       assert(typeof coin.JSONRPC, 'function')
     })
+
+    var jsonrpc = new coin.JSONRPC()
+
+    it('should have getblockcount method', function () {
+      assert(typeof jsonrpc.getblockcount, 'function')
+    })
+
+    it('should have getblockhash method', function () {
+      assert(typeof jsonrpc.getblockhash, 'function')
+    })
+
+    it('should have getblock method', function () {
+      assert(typeof jsonrpc.getblock, 'function')
+    })
+
+    it('should have getrawtransaction method', function () {
+      assert(typeof jsonrpc.getrawtransaction, 'function')
+    })
+
+    it('should have decoderawtransaction method', function () {
+      assert(typeof jsonrpc.decoderawtransaction, 'function')
+    })
+
+    it('should have getrawmempool method', function () {
+      assert(typeof jsonrpc.getrawmempool, 'function')
+    })
+
+    it('should have getpeerinfo method', function () {
+      assert(typeof jsonrpc.getpeerinfo, 'function')
+    })
+
+    it('should have signrawtransaction method', function () {
+      assert(typeof jsonrpc.signrawtransaction, 'function')
+    })
+
+    it('should have gettxout method', function () {
+      assert(typeof jsonrpc.gettxout, 'function')
+    })
+
+    it('should have importprivkey method', function () {
+      assert(typeof jsonrpc.importprivkey, 'function')
+    })
+
+    it('should have sendrawtransaction method', function () {
+      assert(typeof jsonrpc.sendrawtransaction, 'function')
+    })
+
+    it('should have gettxoutsetinfo method', function () {
+      assert(typeof jsonrpc.gettxoutsetinfo, 'function')
+    })
+
+    it('should have getinfo method', function () {
+      assert(typeof jsonrpc.getinfo, 'function')
+    })
+
+    it('should have getbestblockhash method', function () {
+      assert(typeof jsonrpc.getbestblockhash, 'function')
+    })
+
+    it('should have getconnectioncount method', function () {
+      assert(typeof jsonrpc.getconnectioncount, 'function')
+    })
+
+    it('should have getdifficulty method', function () {
+      assert(typeof jsonrpc.getdifficulty, 'function')
+    })
+
+    it('should have stop method', function () {
+      assert(typeof jsonrpc.stop, 'function')
+    })
+
+    it('should have dumpprivkey method', function () {
+      assert(typeof jsonrpc.dumpprivkey, 'function')
+    })
+
   })
+
 })
