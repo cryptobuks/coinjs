0fc92c4646a424c20b201f874617062b7cf3c355
Nathan Marley <nathan.marley@gmail.com>
2014-08-16 15:00:17 -0500

    make tests more colloquial
    use assert.equal to test equality and assert.ok() form of assert() for
    consistency

--- test/test.js
+++ test/test.js
@@ -4,30 +4,30 @@ var assert = require('assert')
 describe('Coin', function () {
   describe('Projects', function () {
     it('should have a Bitcoin project', function () {
-      assert(coin.Projects.bitcoin)
+      assert.ok(coin.Projects.bitcoin)
 
-      assert(coin.Projects.bitcoin.mainnet)
-      assert(coin.Projects.bitcoin.mainnet.port)
-      assert(typeof coin.Projects.bitcoin.mainnet.port, 'number')
+      assert.ok(coin.Projects.bitcoin.mainnet)
+      assert.ok(coin.Projects.bitcoin.mainnet.port)
+      assert.equal(typeof coin.Projects.bitcoin.mainnet.port, 'number')
       assert.equal(coin.Projects.bitcoin.mainnet.port, 8332)
 
-      assert(coin.Projects.bitcoin.testnet)
-      assert(coin.Projects.bitcoin.testnet.port)
-      assert(typeof coin.Projects.bitcoin.testnet.port, 'number')
+      assert.ok(coin.Projects.bitcoin.testnet)
+      assert.ok(coin.Projects.bitcoin.testnet.port)
+      assert.equal(typeof coin.Projects.bitcoin.testnet.port, 'number')
       assert.equal(coin.Projects.bitcoin.testnet.port, 18332)
     })
 
     it('should have a Litecoin project', function () {
-      assert(coin.Projects.litecoin)
+      assert.ok(coin.Projects.litecoin)
 
-      assert(coin.Projects.litecoin.mainnet)
-      assert(coin.Projects.litecoin.mainnet.port)
-      assert(typeof coin.Projects.litecoin.mainnet.port, 'number')
+      assert.ok(coin.Projects.litecoin.mainnet)
+      assert.ok(coin.Projects.litecoin.mainnet.port)
+      assert.equal(typeof coin.Projects.litecoin.mainnet.port, 'number')
       assert.equal(coin.Projects.litecoin.mainnet.port, 9332)
 
-      assert(coin.Projects.litecoin.testnet)
-      assert(coin.Projects.litecoin.testnet.port)
-      assert(typeof coin.Projects.litecoin.testnet.port, 'number')
+      assert.ok(coin.Projects.litecoin.testnet)
+      assert.ok(coin.Projects.litecoin.testnet.port)
+      assert.equal(typeof coin.Projects.litecoin.testnet.port, 'number')
       assert.equal(coin.Projects.litecoin.testnet.port, 19332)
     })
 
@@ -35,81 +35,81 @@ describe('Coin', function () {
 
   describe('JSONRPC', function () {
     it('should have a JSONRPC() function', function () {
-      assert(typeof coin.JSONRPC, 'function')
+      assert.ok(typeof coin.JSONRPC, 'function')
     })
 
     var jsonrpc = new coin.JSONRPC()
 
     it('should have getblockcount method', function () {
-      assert(typeof jsonrpc.getblockcount, 'function')
+      assert.equal(typeof jsonrpc.getblockcount, 'function')
     })
 
     it('should have getblockhash method', function () {
-      assert(typeof jsonrpc.getblockhash, 'function')
+      assert.equal(typeof jsonrpc.getblockhash, 'function')
     })
 
     it('should have getblock method', function () {
-      assert(typeof jsonrpc.getblock, 'function')
+      assert.equal(typeof jsonrpc.getblock, 'function')
     })
 
     it('should have getrawtransaction method', function () {
-      assert(typeof jsonrpc.getrawtransaction, 'function')
+      assert.equal(typeof jsonrpc.getrawtransaction, 'function')
     })
 
     it('should have decoderawtransaction method', function () {
-      assert(typeof jsonrpc.decoderawtransaction, 'function')
+      assert.equal(typeof jsonrpc.decoderawtransaction, 'function')
     })
 
     it('should have getrawmempool method', function () {
-      assert(typeof jsonrpc.getrawmempool, 'function')
+      assert.equal(typeof jsonrpc.getrawmempool, 'function')
     })
 
     it('should have getpeerinfo method', function () {
-      assert(typeof jsonrpc.getpeerinfo, 'function')
+      assert.equal(typeof jsonrpc.getpeerinfo, 'function')
     })
 
     it('should have signrawtransaction method', function () {
-      assert(typeof jsonrpc.signrawtransaction, 'function')
+      assert.equal(typeof jsonrpc.signrawtransaction, 'function')
     })
 
     it('should have gettxout method', function () {
-      assert(typeof jsonrpc.gettxout, 'function')
+      assert.equal(typeof jsonrpc.gettxout, 'function')
     })
 
     it('should have importprivkey method', function () {
-      assert(typeof jsonrpc.importprivkey, 'function')
+      assert.equal(typeof jsonrpc.importprivkey, 'function')
     })
 
     it('should have sendrawtransaction method', function () {
-      assert(typeof jsonrpc.sendrawtransaction, 'function')
+      assert.equal(typeof jsonrpc.sendrawtransaction, 'function')
     })
 
     it('should have gettxoutsetinfo method', function () {
-      assert(typeof jsonrpc.gettxoutsetinfo, 'function')
+      assert.equal(typeof jsonrpc.gettxoutsetinfo, 'function')
     })
 
     it('should have getinfo method', function () {
-      assert(typeof jsonrpc.getinfo, 'function')
+      assert.equal(typeof jsonrpc.getinfo, 'function')
     })
 
     it('should have getbestblockhash method', function () {
-      assert(typeof jsonrpc.getbestblockhash, 'function')
+      assert.equal(typeof jsonrpc.getbestblockhash, 'function')
     })
 
     it('should have getconnectioncount method', function () {
-      assert(typeof jsonrpc.getconnectioncount, 'function')
+      assert.equal(typeof jsonrpc.getconnectioncount, 'function')
     })
 
     it('should have getdifficulty method', function () {
-      assert(typeof jsonrpc.getdifficulty, 'function')
+      assert.equal(typeof jsonrpc.getdifficulty, 'function')
     })
 
     it('should have stop method', function () {
-      assert(typeof jsonrpc.stop, 'function')
+      assert.equal(typeof jsonrpc.stop, 'function')
     })
 
     it('should have dumpprivkey method', function () {
-      assert(typeof jsonrpc.dumpprivkey, 'function')
+      assert.equal(typeof jsonrpc.dumpprivkey, 'function')
     })
 
   })
