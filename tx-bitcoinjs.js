b1463b82a17f381633ca0469c01b7e7b2dfed177
Nathan Marley <nathan.marley@gmail.com>
2014-09-19 18:52:20 -0500

    add var keyword to prevent usage as globals

--- tx-bitcoinjs.js
+++ tx-bitcoinjs.js
@@ -1,7 +1,7 @@
 var Bitcoin = require('bitcoinjs-lib')
 
 // create a new transaction object
-tx = new Bitcoin.Transaction()
+var tx = new Bitcoin.Transaction()
 
 // reference an unspent output from a different tx as an input to this tx
 tx.addInput('0ae0096056aaf80bb5b22c2624d7c278bbde9dfaf15fe3761b48d6b2edb41950', 0)
@@ -13,7 +13,7 @@ tx.addOutput('mmH11CDMMcm9y8Q3sFmELTWKExTZY15JFY', 10000000)
 tx.addOutput('mu6tjsNNesrUkcUVi8y7kVGGxHtjXJoREt', 180000000)
 
 // initialize a private key from WIF
-key = Bitcoin.ECKey.fromWIF('92KEQmqc41iqRnEPn6d1FmdJmNWNTV8t8qCivBiGeuogRXSTNkd')
+var key = Bitcoin.ECKey.fromWIF('92KEQmqc41iqRnEPn6d1FmdJmNWNTV8t8qCivBiGeuogRXSTNkd')
 
 // Sign the 1st (and only) input in the tx
 tx.sign(0, key)
