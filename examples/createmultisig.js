12e22bc5c322704c69bf9f1cd09842bf05a9f422
Nathan Marley <nathan.marley@gmail.com>
2014-09-19 22:09:42 -0500

    remove unused var

--- examples/createmultisig.js
+++ examples/createmultisig.js
@@ -1,8 +1,6 @@
 var jsonrpc = require('./current')
 var util = require('util')
 
-var rawtx = process.argv[2];
-
 var pubkey1 = '03d4524af66b01acf36445eda7f44572227b4e31fb47b9cacc4f5e263ccb7f69fa';
 var pubkey2 = '034db53de97b98424a8bf9f4d5f3058581bb0d7dae80b2cd09d30594d74956b48b';
 
