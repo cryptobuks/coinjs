c6902f41bbede9a0991bd2b75510cc94f27f675d
Nathan Marley <nathan.marley@gmail.com>
2014-10-02 09:31:04 -0500

    sort pubkeys before creating multisig address

--- bitcoinjs-create-multisig.js
+++ bitcoinjs-create-multisig.js
@@ -7,7 +7,7 @@ var util = require('util')
 var pubkeys_hex =
   [ "02547194cd5dd237ea0c067e4f251b04b9243cd8bcf5997360d581fe6fc32381df",
     "03f3f26dc67491505083ced8e03fee1d27336b6a4d6fbd5a24efe04e95597c137e",
-    "02b9e28ba8e285752cabaee82d99e0378cea9417801ca3fb499a6506caee480ca6" ]
+    "02b9e28ba8e285752cabaee82d99e0378cea9417801ca3fb499a6506caee480ca6" ].sort()
 var pubkeys = pubkeys_hex.map(bitcoin.ECPubKey.fromHex)
 var redeemScript = scripts.multisigOutput(2, pubkeys)
 
